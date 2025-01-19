b36b360a058207477f8466a9c38ce8b546dac8c6
{your_username} <{your_email}>
2025-01-19 12:07:05 +0530

    openstreet map add

--- components/contact/mapsection.tsx
+++ components/contact/mapsection.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import L from "leaflet";
 import * as motion from "motion/react-client";
 
-function Mapsection() {
+// Required CSS for Leaflet
+import "leaflet/dist/leaflet.css";
+
+const Mapsection = () => {
+  // Coordinates for the marker
+  const center: [number, number] = [30.296611, 78.018083];
+
   const textAnimation = {
     hidden: { opacity: 0, y: 50 },
     visible: { opacity: 1, y: 0 },
@@ -12,8 +20,21 @@ function Mapsection() {
     visible: { opacity: 1, scale: 1 },
   };
 
-  const locationUrl =
-    "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3444.910441766923!2d78.01813890000001!3d30.2966111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1737267664098!5m2!1sen!2sin&markers=30.296611,78.018083"; // Added marker
+  // Fixing default Leaflet marker image issue (with React + Leaflet)
+  const markerIcon = new L.Icon({
+    iconUrl: require("leaflet/dist/images/marker-icon.png"), // Default marker icon
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -41],
+  });
+
+  // Google Maps URL for the location (updated to use your provided URL)
+  const googleMapsUrl = `https://maps.app.goo.gl/3oywQKzBHSaHqtn79`;
+
+  // Function to handle marker click and redirect to Google Maps
+  const handleMarkerClick = () => {
+    window.open(googleMapsUrl, "_blank");
+  };
 
   return (
     <motion.div
@@ -30,13 +51,34 @@ function Mapsection() {
         transition={{ duration: 0.8 }}
         variants={mapAnimation}
       >
-        <iframe
-          src={locationUrl}
-          width="600"
-          height="450"
-          loading="lazy"
-          style={{ border: 0 }}
-        ></iframe>
+        <MapContainer
+          center={center}
+          zoom={15}
+          scrollWheelZoom={false}
+          style={{ width: "100%", height: "500px" }}
+        >
+          {/* Tile Layer (using OpenStreetMap) */}
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          {/* Marker with Tooltip (permanent title above marker) */}
+          <Marker
+            position={center}
+            icon={markerIcon}
+            eventHandlers={{ click: handleMarkerClick }}
+          >
+            <Tooltip permanent direction="top">
+              Phyoli Health Care
+            </Tooltip>
+            <Popup>
+              Phyoli Health Care
+              <br />
+              Dehradun, Uttarakhand 248007
+            </Popup>
+          </Marker>
+        </MapContainer>
       </motion.div>
 
       {/* Information Section */}
@@ -71,6 +113,6 @@ function Mapsection() {
       </motion.div>
     </motion.div>
   );
-}
+};
 
 export default Mapsection;
